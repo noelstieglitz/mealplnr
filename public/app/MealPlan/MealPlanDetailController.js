f678f9884ddbaa165eecca02f7776faa9df5b7df
Noel Stieglitz <noel@componotech.com>
2015-12-19 16:11:39 -0500

    general layout

--- public/app/MealPlan/MealPlanDetailController.js
+++ public/app/MealPlan/MealPlanDetailController.js
@@ -6,7 +6,7 @@ MealPlanDetailController.$inject = ['mealPlanService', 'mealPlan'];
 
 function MealPlanDetailController(mealPlanService, mealPlan) {
   var vm = this;
-  vm.mealPlan = mealPlan;
-  vm.week = moment(vm.mealPlan.startDate).format("MMMM Do YYYY");
+  vm.meals = mealPlan.meals;
+  vm.week = moment(mealPlan.startDate).format("MMMM Do YYYY");
 }
 })();
