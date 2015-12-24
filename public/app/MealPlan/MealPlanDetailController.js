2af0d1e645db23503b0fe338b984436bf9f7ee98
Noel Stieglitz <noel@componotech.com>
2015-12-23 19:13:03 -0500

    veggie/carbs on meal plan details.

--- public/app/MealPlan/MealPlanDetailController.js
+++ public/app/MealPlan/MealPlanDetailController.js
@@ -6,7 +6,17 @@
 
     function MealPlanDetailController(mealPlanService, mealPlan) {
         var vm = this;
-        vm.meals = mealPlan.meals;
+        vm.meals = [];
+        angular.forEach(mealPlan.meals, function(m, i){
+            debugger;
+            this[i] = m;
+            if(m.veggie && m.carb){
+                this[i].servedWithText = "Served with " + m.veggie + " and " + m.carb;
+            }
+            else if(m.veggie || m.carb){
+                this[i].servedWithText = "Served with " + (m.veggie || m.carb);
+            }
+        }, vm.meals);
         vm.week = moment(mealPlan.startDate).format("MMMM Do YYYY");
     }
 })();
